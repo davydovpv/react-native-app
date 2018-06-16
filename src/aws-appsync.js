75c92c3cd9c626b30be797ba04c0be3203947799
lifeinsure <david@demadnage.com>
2018-06-16 15:44:16 -0700

    Backend connected via AppSync+GraphQL, New Welcome Flow

--- src/aws-appsync.js
+++
@@ -1,6 +0,0 @@
-﻿export default {
-    "graphqlEndpoint": "https://hyhhf2doo5exxohzirc66e4rbe.appsync-api.us-east-2.amazonaws.com/graphql",
-    "region": "us-east-2",
-    "authenticationType": "API_KEY",
-    "apiKey": "da2-diqsdmxpbfb2zmmndyf6a5rw7m"
-}
